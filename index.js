745089d8e768404d5a87775f707725b2cf4ba7bd
shivam kumar <20051252@kiit.ac.in>
2023-11-28 21:17:52 +0530

    dummy push

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ app.use(bodyParser.json());
 // const webhookSecret = process.env.GITHUB_WEBHOOK_SECRET;
 
 app.post('/webhook', (req, res) => {
-    console.log('req', req);
-    console.log('payload', req.payload);
-    console.log('webhooook called sucessfully');
+    // console.log('req', req);
+    // console.log('payload', req.payload);
+    console.log('webhook called sucessfully');
 //   const event = req.headers['x-github-event'];
 //   const signature = req.headers['x-hub-signature'];
 
