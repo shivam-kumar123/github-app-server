8d4f26f7670c3209f98f65189c88bb7046a5a3a0
shivam kumar <75497119+shivam-kumar123@users.noreply.github.com>
2023-11-29 13:08:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "*", // Allow all origins for now
+    origin: "*", 
     methods: ["GET", "POST"]
   }
 });
